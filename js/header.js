e13249e378496f2d7122795cfdf60e3ea72c707e
Yen <han080718@gmail.com>
2023-04-23 17:56:59 +0700

    modify files

--- js/header.js
+++
@@ -1,8 +0,0 @@
-const togglebtn = document.querySelector('.navbar_toglebtn');
-const menu = document.querySelector('.navbar_menu');
-const icons = document.querySelector('.nav_wrap2');
-
-togglebtn.addEventListener('click', ()=>{
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-});
